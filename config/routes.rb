049fc4521e0d22b8f53d663fae0ea1abd0de540a
tomoharu1205 <tomo611205434@gamil.com>
2019-07-28 15:14:17 +0900

    summay

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'messages#index'
-  resources :messages, only: [:edit, :update]
+  root 'groups#index'
+  resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update]
     resources :messages, only: [:index, :create]
 end
