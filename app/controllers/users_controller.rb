ce5d2a00c02190741453985c19555e5d369c1c0d
tomoharu1205 <tomo611205434@gamil.com>
2019-08-20 20:35:59 +0900

    summay

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,13 @@ class UsersController < ApplicationController
 
   def index
     
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    @users = User.where("name LIKE(?) and id != ?", "%#{params[:members]}%", "#{current_user.id}").limit(20)
     respond_to do |format|
       format.html
       format.json
     end
   end
-
+ 
   def edit 
   end
   
