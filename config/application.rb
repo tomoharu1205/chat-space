9e0528a5886946ceb1f6b06edeac99775dba6f9c
tomoharu1205 <tomo611205434@gamil.com>
2019-09-06 21:01:00 +0900

    日本時間に修正

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,14 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
+    
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
       g.helper false
       g.test_framework false
-      config.i18n.default_locale = :ja
     end
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
   end
 end
