7449bf93b1f2acd840e46d546d7cbce5e48cf19a
tomoharu1205 <tomo611205434@gamil.com>
2019-09-06 18:29:18 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -63,7 +63,6 @@ $(function(){
       data: {id: last_message_id}
     })
     .done(function(messages) {
-      console.log("ooook");
       //追加するHTMLの入れ物を作る
       var insertHTML = '';
       //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
